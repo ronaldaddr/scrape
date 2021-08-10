28d2f522eb2018bf6a1a307fc9b101fcf3adb4d6
ronald adriansyah <adrian_syah78@yahoo.co.id>
2021-08-10 09:20:56 +0700

    Update 1688api2.py

--- 1688api2.py
+++ 1688api2.py
@@ -13,8 +13,8 @@ import json
 from bs4 import BeautifulSoup
 import os
 driver= webdriver.PhantomJS()
-#url = 'https://detail.1688.com/offer/650635541901.html?spm=a260k.dacugeneral.home2019rec.6.6633436cLYnbqX&tracelog=p4p&clickid=b7ad1f4f0aff41bca346974460c0cbf1&sessionid=134b5a5d503cc164298709a41b8b3775'
-url = 'https://detail.1688.com/offer/649838054631.html'
+url = 'https://detail.1688.com/offer/650635541901.html?spm=a260k.dacugeneral.home2019rec.6.6633436cLYnbqX&tracelog=p4p&clickid=b7ad1f4f0aff41bca346974460c0cbf1&sessionid=134b5a5d503cc164298709a41b8b3775'
+#url = 'https://detail.1688.com/offer/649838054631.html'
 
 injected_javascript = (
     'document.write(JSON.stringify(iDetailConfig, undefined, 4));document.write(JSON.stringify(iDetailData, undefined, 4));'
